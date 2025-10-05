2dc25506067975dcb99024413873a62c8e7315bd
sourbhryadav1 <sourbhr12@gmail.com>
2025-10-05 19:36:40 +0530

    added env in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiBaseUrl = (process.env.VITE_API_URL || "http://localhost:5001") + "/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",  // base url for all the requests
+    baseURL: apiBaseUrl,
 });
 
 export default api;
