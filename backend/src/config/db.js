cbc5fd73c4be8941354607511b1c21fe0d1447e2
sourbhryadav1 <sourbhr12@gmail.com>
2025-10-05 18:17:57 +0530

    updated mongo fields

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("Mongo connected!");
 
